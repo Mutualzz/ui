e140dd4f380789a0459de9e63a8a6403764f399b
Mateie <contact@mateie.dev>
2025-04-25 13:01:11 -0700

    fixed sortThemes types

--- src/utils/sortThemes.ts
+++ src/utils/sortThemes.ts
@@ -1,12 +1,10 @@
-import type { Theme, Themes } from "@mutualzz/ui/types";
+import type { Theme } from "@mutualzz/ui/types";
 
 export const sortThemes = (themes: Theme[]): Theme[] => {
-    const priorityOrder: Themes[] = ["baseDark", "baseLight"];
+    const priorityOrder: string[] = ["baseDark", "baseLight"];
 
     return [
-        ...themes.filter((theme) => priorityOrder.includes(theme.id as Themes)),
-        ...themes.filter(
-            (theme) => !priorityOrder.includes(theme.id as Themes),
-        ),
+        ...themes.filter((theme) => priorityOrder.includes(theme.id)),
+        ...themes.filter((theme) => !priorityOrder.includes(theme.id)),
     ];
 };
