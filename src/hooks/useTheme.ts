5460fdc662a9ed7adbd5affc6e342035fb23320f
Mateie <contact@mateie.dev>
2025-04-25 21:27:45 -0700

    fixed the issue with paper (phew)

--- src/hooks/useTheme.ts
+++ src/hooks/useTheme.ts
@@ -1,7 +1,8 @@
 import { ThemeContext } from "@root/ThemeProvider";
+import { type ThemeContextInterface } from "@root/types";
 import { useContext } from "react";
 
-export const useTheme = () => {
+export const useTheme = (): ThemeContextInterface => {
     const context = useContext(ThemeContext);
     if (!context)
         throw new Error("useTheme must be used within a ThemeProvider");
