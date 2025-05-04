3e40ad6ed1ff08109ff0028f450aee5328b71f71
Mateie <contact@mateie.dev>
2025-05-04 00:08:29 -0700

    Fixed paper types made it into a styled from emotio

--- src/components/surfaces/Paper/Paper.tsx
+++ src/components/surfaces/Paper/Paper.tsx
@@ -1,85 +1,39 @@
-import { useTheme } from "@root/hooks/useTheme";
+import styled from "@emotion/styled";
 import { dynamicElevation } from "@root/utils/dynamicElevation";
-import type { FC } from "react";
 import type { PaperProps } from "./Paper.types";
 
-export const Paper: FC<PaperProps> = ({
-    display = "flex",
-    direction,
-    wrap,
-    justifyContent,
-    alignItems,
-    alignContent,
-    spacing,
-    p,
-    m,
-    order,
-    grow,
-    shrink,
-    basis,
-    flex,
-    alignSelf,
-    pt,
-    pr,
-    pb,
-    pl,
-    mt,
-    mr,
-    mb,
-    ml,
-    px,
-    py,
-    mx,
-    my,
-    elevation = 0,
-    children,
-    ...props
-}) => {
-    if (elevation < 0 || elevation > 4)
-        throw new Error("Elevation must be between 0 and 4");
-    const { theme } = useTheme();
-
-    return (
-        <div
-            css={{
-                backgroundColor: dynamicElevation(
-                    theme.colors.surface,
-                    elevation,
-                ),
-                boxShadow: `0 ${elevation + 1}px ${elevation * 2}px rgba(0,0,0,${elevation * 0.1})`,
-                borderRadius: "0.75rem",
-                transition: "all 0.2 ease",
-                display,
-                flexDirection: direction,
-                justifyContent,
-                alignItems,
-                alignContent,
-                flexWrap: wrap,
-                gap: spacing,
-                padding: p,
-                margin: m,
-                order,
-                flexGrow: grow,
-                flexShrink: shrink,
-                flexBasis: basis,
-                flex,
-                alignSelf,
-                paddingTop: pt,
-                paddingRight: pr,
-                paddingBottom: pb,
-                paddingLeft: pl,
-                marginTop: mt,
-                marginRight: mr,
-                marginBottom: mb,
-                marginLeft: ml,
-                paddingBlock: py,
-                paddingInline: px,
-                marginBlock: my,
-                marginInline: mx,
-            }}
-            {...props}
-        >
-            {children}
-        </div>
-    );
-};
+export const Paper = styled("div")<PaperProps>`
+    background-color: ${({ theme, elevation = 0 }) =>
+        dynamicElevation(theme.colors.surface, elevation)};
+    box-shadow: ${({ elevation = 0 }) =>
+        `0 ${elevation + 1}px ${elevation * 2}px rgba(0,0,0,${elevation * 0.1})`};
+    border-radius: 0.75rem;
+    transition: all 0.2 ease;
+    display: ${({ display }) => display};
+    flex-direction: ${({ direction }) => direction};
+    justify-content: ${({ justifyContent }) => justifyContent};
+    align-items: ${({ alignItems }) => alignItems};
+    align-content: ${({ alignContent }) => alignContent};
+    flex-wrap: ${({ wrap }) => wrap};
+    gap: ${({ spacing }) => spacing};
+    padding: ${({ p }) => p};
+    margin: ${({ m }) => m};
+    order: ${({ order }) => order};
+    flex-grow: ${({ grow }) => grow};
+    flex-shrink: ${({ shrink }) => shrink};
+    flex-basis: ${({ basis }) => basis};
+    flex: ${({ flex }) => flex};
+    align-self: ${({ alignSelf }) => alignSelf};
+    padding-top: ${({ pt }) => pt};
+    padding-right: ${({ pr }) => pr};
+    padding-bottom: ${({ pb }) => pb};
+    padding-left: ${({ pl }) => pl};
+    margin-top: ${({ mt }) => mt};
+    margin-right: ${({ mr }) => mr};
+    margin-bottom: ${({ mb }) => mb};
+    margin-left: ${({ ml }) => ml};
+    padding-block: ${({ py }) => py};
+    padding-inline: ${({ px }) => px};
+    margin-block: ${({ my }) => my};
+    margin-inline: ${({ mx }) => mx};
+`;
--- src/components/surfaces/Paper/Paper.types.ts
+++ src/components/surfaces/Paper/Paper.types.ts
@@ -1,7 +1,10 @@
-import type { StackProps } from "../../layout/Stack/Stack.types";
+import { BoxProps } from "@root/components/layout/Box/Box.types";
+import { StackProps } from "@root/components/layout/Stack/Stack.types";
 
-export type PaperElevation = 0 | 1 | 2 | 3 | 4;
+export type PaperElevation = number;
 
-export interface PaperProps extends StackProps {
+export interface PaperProps extends Omit<StackProps, "display"> {
+    display: BoxProps["display"] | StackProps["display"];
+
     elevation?: PaperElevation;
 }
