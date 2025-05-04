316bb4c0c96283fe895aad953056e277d86984b8
Mateie <contact@mateie.dev>
2025-05-03 18:51:21 -0700

    removed sx prop

--- src/emotion.d.ts
+++ src/emotion.d.ts
@@ -1,17 +1,6 @@
 import "@emotion/react";
-import { Interpolation } from "@emotion/react";
 import { type Theme as MzTheme } from "@ui/types";
 
 declare module "@emotion/react" {
     export type Theme = MzTheme;
-
-    export interface StyledComponent<
-        Props extends {} = {},
-        ThemeType extends Theme = Theme,
-    > extends React.FC<
-            Props &
-                ComponentPropsWithRef<any> & {
-                    sx?: Interpolation<ThemeType> | Interpolation<ThemeType>[];
-                }
-        > {}
 }
