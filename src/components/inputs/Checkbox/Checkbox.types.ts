8f24f66136fadb6977f5a5ae3298cc9e9cb08940
Mateie <contact@mateie.dev>
2025-05-06 03:56:35 -0700

    added checkbox group and rtl feature

--- src/components/inputs/Checkbox/Checkbox.types.ts
+++ src/components/inputs/Checkbox/Checkbox.types.ts
@@ -1,7 +1,8 @@
-import type { HTMLAttributes, ReactNode } from "react";
+import type { ChangeEvent, InputHTMLAttributes, ReactNode } from "react";
 import { Color, ColorLike, Size, Variant } from "../../../types";
 
-export interface CheckboxProps extends HTMLAttributes<HTMLInputElement> {
+export interface CheckboxProps
+    extends Omit<InputHTMLAttributes<HTMLInputElement>, "size" | "type"> {
     label?: ReactNode;
     checked?: boolean;
     defaultChecked?: boolean;
@@ -16,4 +17,16 @@ export interface CheckboxProps extends HTMLAttributes<HTMLInputElement> {
     color?: Color | ColorLike;
     size?: Size | number;
     variant?: Variant;
+
+    rtl?: boolean;
+}
+
+export interface CheckboxGroupProps {
+    name: string;
+    value?: string[];
+    defaultValue?: string[];
+    onChange?: (event: ChangeEvent<HTMLInputElement>, value: string[]) => void;
+    disabled?: boolean;
+    row?: boolean;
+    children: ReactNode;
 }
