2b6ce655a1194ad14b4ccc1e65dbda7ede7efa55
mateie <mateie@gmail.com>
2025-05-05 17:02:31 -0700

    removed obsolete util for typography

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -3,7 +3,6 @@ import { blendOver } from "./blendOver";
 import { dynamicElevation } from "./dynamicElevation";
 import { invertColor } from "./invertColor";
 import { isThemeColor } from "./isThemeColor";
-import { isTypographyColor } from "./isTypographyColor";
 import { randomHexColor } from "./randomHexColor";
 import { readableTextColor } from "./readableTextColor";
 import { sortThemes } from "./sortThemes";
@@ -14,7 +13,6 @@ export {
     dynamicElevation,
     invertColor,
     isThemeColor,
-    isTypographyColor,
     randomHexColor,
     readableTextColor,
     sortThemes,
