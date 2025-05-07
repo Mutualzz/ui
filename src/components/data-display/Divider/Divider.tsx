3677a1ca992876d190735377d7b5af1450d806ce
Mateie <contact@mateie.dev>
2025-05-06 21:49:20 -0700

    Fixed useTheme import path

--- src/components/data-display/Divider/Divider.tsx
+++ src/components/data-display/Divider/Divider.tsx
@@ -1,7 +1,7 @@
 import styled from "@emotion/styled";
 import type { FC } from "react";
 
-import { useTheme } from "hooks/useTheme";
+import { useTheme } from "../../../hooks/useTheme";
 
 import { resolveDividerColor, resolveDividerVariant } from "./Divider.helpers";
 import type { DividerProps, DividerVariant } from "./Divider.types";
