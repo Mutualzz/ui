30c02efd65467caf265e8fc4e12d891c3195dc3c
Mateie <contact@mateie.dev>
2025-05-14 23:06:01 -0700

    Added weight option to typography

--- src/components/data-display/Typography/Typography.tsx
+++ src/components/data-display/Typography/Typography.tsx
@@ -4,9 +4,16 @@ import { variantStyles } from "./Typography.helpers";
 import { type TypographyProps } from "./Typography.types";
 
 export const Typography = styled("span")<TypographyProps>(
-    ({ theme, level = "body-md", color = "#fff", variant = "none" }) => ({
+    ({
+        theme,
+        level = "body-md",
+        color = "#fff",
+        variant = "none",
+        weight,
+    }) => ({
         ...variantStyles(theme, color)[variant],
         ...theme.typography.levels[level],
         padding: 8,
+        fontWeight: weight,
     }),
 );
--- src/components/data-display/Typography/Typography.types.ts
+++ src/components/data-display/Typography/Typography.types.ts
@@ -5,11 +5,13 @@ import {
     type TypographyLevel,
     type Variant,
 } from "../../../types";
+import type { FontWeight } from "../../../types/Typography.props";
 
 export type TypographyVariant = Variant | "none";
 
 export interface TypographyProps extends HTMLAttributes<HTMLSpanElement> {
     level?: TypographyLevel;
+    weight?: FontWeight;
     color?: Color | ColorLike;
     variant?: TypographyVariant;
 }
