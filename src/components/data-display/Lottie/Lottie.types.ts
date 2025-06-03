d9483581f81014d9cb9f46731253931f8619a2c8
Mateie <contact@mateie.dev>
2025-06-02 19:19:53 -0700

    Removed lottie (not needed)

--- src/components/data-display/Lottie/Lottie.types.ts
+++
@@ -1,7 +0,0 @@
-import type { HTMLAttributes } from "react";
-
-export interface LottieProps extends HTMLAttributes<HTMLDivElement> {
-    path: any;
-    loop?: boolean;
-    autoplay?: boolean;
-}
