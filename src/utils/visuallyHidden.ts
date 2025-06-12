49d961db7c62a8541fc5f0f09a70f43f2cb76af3
Mateie <contact@mateie.dev>
2025-06-12 02:38:22 -0700

    CSSObjectified

--- src/utils/visuallyHidden.ts
+++ src/utils/visuallyHidden.ts
@@ -3,13 +3,13 @@ import type { CSSObject } from "@emotion/react";
 const visuallyHidden: CSSObject = {
     border: 0,
     clip: "rect(0 0 0 0)",
-    height: "1px",
-    margin: "-1px",
+    height: 1,
+    margin: -1,
     overflow: "hidden",
     padding: 0,
     position: "absolute",
     whiteSpace: "nowrap",
-    width: "1px",
+    width: 1,
 };
 
 export default visuallyHidden;
