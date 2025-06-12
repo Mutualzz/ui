eb5e9537c2749199b18a1dbab069600f9f99e92b
Mateie <contact@mateie.dev>
2025-06-12 02:37:53 -0700

    Switched CSSProperties with CSSObject

--- src/utils/visuallyHidden.ts
+++ src/utils/visuallyHidden.ts
@@ -1,6 +1,6 @@
-import type { CSSProperties } from "react";
+import type { CSSObject } from "@emotion/react";
 
-const visuallyHidden: CSSProperties = {
+const visuallyHidden: CSSObject = {
     border: 0,
     clip: "rect(0 0 0 0)",
     height: "1px",
