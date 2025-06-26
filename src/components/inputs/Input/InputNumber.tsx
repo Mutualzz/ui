4bab6b1e6a5336817d3b94c7503647ebb9eaa0e3
Azrael <azrael@mutualzz.com>
2025-06-26 08:51:38 -0700

    Fixed input number again :3

--- src/components/inputs/Input/InputNumber.tsx
+++ src/components/inputs/Input/InputNumber.tsx
@@ -1,4 +1,4 @@
-import { useRef } from "react";
+import { useRef, type ClipboardEvent, type KeyboardEvent } from "react";
 import type { InputNumberProps } from "./Input.types";
 import { DecoratorWrapper, InputBase, InputRoot } from "./InputBase";
 
@@ -22,6 +22,52 @@ export const InputNumber = ({
 }: InputNumberProps) => {
     const inputRef = useRef<HTMLInputElement>(null);
 
+    const handleKeyDown = (e: KeyboardEvent<HTMLInputElement>) => {
+        const { key, ctrlKey, metaKey, currentTarget } = e;
+        const cursorPos = currentTarget.selectionStart ?? 0;
+        const { value } = currentTarget;
+
+        const allowedModifierKeys = [
+            "Backspace",
+            "Delete",
+            "Tab",
+            "Escape",
+            "Enter",
+            "ArrowLeft",
+            "ArrowRight",
+            "ArrowUp",
+            "ArrowDown",
+            "Home",
+            "End",
+        ];
+
+        const isCtrlOrMetaKey = ctrlKey || metaKey;
+        const isDigit = /^\d$/.test(key);
+        const isMinus = key === "-";
+        const isDot = key === ".";
+
+        if (
+            allowedModifierKeys.includes(key) ||
+            isDigit ||
+            (isCtrlOrMetaKey && key.toLowerCase() === "a") ||
+            (isCtrlOrMetaKey && key.toLowerCase() === "c") || // copy
+            (isCtrlOrMetaKey && key.toLowerCase() === "v") || // paste
+            (isCtrlOrMetaKey && key.toLowerCase() === "x") // cut
+        )
+            return;
+
+        if (isMinus && cursorPos === 0 && !value.includes("-")) return;
+
+        if (isDot && !value.includes(".")) return;
+
+        e.preventDefault();
+    };
+
+    const handlePaste = (e: ClipboardEvent<HTMLInputElement>) => {
+        const pasted = e.clipboardData.getData("text");
+        if (!/^-?\d*(\.\d*)?$/.test(pasted.trim())) e.preventDefault();
+    };
+
     return (
         <InputRoot
             color={color}
@@ -44,6 +90,8 @@ export const InputNumber = ({
                 min={min}
                 max={max}
                 step={step}
+                onKeyDown={handleKeyDown}
+                onPaste={handlePaste}
             />
 
             {endDecorator && (
