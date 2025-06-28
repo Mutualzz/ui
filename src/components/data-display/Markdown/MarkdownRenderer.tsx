cbc09e280d12d684a6ef2cec222d81e6e1c6f4c6
Azrael <azrael@mutualzz.com>
2025-06-28 11:48:19 -0700

    Moved Markdown to the App locally (better)

--- src/components/data-display/Markdown/MarkdownRenderer.tsx
+++
@@ -1,9 +0,0 @@
-import { useMemo } from "react";
-import { parseBlocks } from "../../../utils/markdown/parseBlocks";
-import { Box } from "../../layout/Box/Box";
-import { renderBlock } from "./renderBlock";
-
-export const MarkdownRenderer = ({ children }: { children: string }) => {
-    const blocks = useMemo(() => parseBlocks(children), [children]);
-    return <Box>{blocks.map((block, i) => renderBlock(block, i))}</Box>;
-};
