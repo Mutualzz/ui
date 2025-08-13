d2f6a230f6e3518fc720688d764654be9fc6244c
mateie <mateie@gmail.com>
2025-08-13 14:32:40 -0700

    Extended BoxProps from HTMLDivElement

--- src/components/Box/Box.types.ts
+++ src/components/Box/Box.types.ts
@@ -1,7 +1,9 @@
+import type { HTMLAttributes } from "react";
+
 /**
  * Props for the Box component.
  */
-export interface BoxProps {
+export interface BoxProps extends HTMLAttributes<HTMLDivElement> {
     /**
      * If true, the Box will be displayed as an inline-block element; otherwise, it will be a block element.
      */
