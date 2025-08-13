504b30f9ba7a1e45d12c77668dbc9634aa9824ef
mateie <mateie@gmail.com>
2025-08-13 14:43:26 -0700

    tweaked and added colortypes

--- src/hooks/useColorInput.ts
+++ src/hooks/useColorInput.ts
@@ -1,65 +1,16 @@
-import type { ColorLike } from "@ui-types";
-import { formatHex, formatHex8, parse } from "culori";
+import type {
+    ColorLike,
+    ColorType,
+    Hex,
+    HSL,
+    HSLA,
+    RGB,
+    RGBA,
+} from "@ui-types";
+import { isValidColorInput } from "@utils/colorRegex";
+import { randomColor } from "@utils/randomColor";
+import { formatHex, formatHex8, formatHsl, formatRgb, parse } from "culori";
 import { useState } from "react";
-import { alpha as alphaUtil, randomHexColor } from "../utils";
-
-/**
- * Checls if the input is a valid hex color.
- */
-const isValidHex = (value: string): boolean =>
-    /^#([0-9a-fA-F]{3,4}|[0-9a-fA-F]{6}|[0-9a-fA-F]{8})$/.test(value.trim());
-
-/**
- * Checks if the input is a valid RGB color.
- * RGB format: rgb(r, g, b)
- * where r, g, b are integers between 0 and 255.
- */
-const isValidRgb = (value: string): boolean =>
-    /^rgb\(\s*\d{1,3}\s*,\s*\d{1,3}\s*,\s*\d{1,3}\s*\)$/.test(value.trim());
-
-/**
- * Checks if the input is a valid RGBA color.
- * RGBA format: rgba(r, g, b, a)
- * where r, g, b are integers between 0 and 255,
- * and a is a number between 0 and 1 (inclusive).
- */
-const isValidRgba = (value: string): boolean =>
-    /^rgba\(\s*\d{1,3}\s*,\s*\d{1,3}\s*,\s*\d{1,3}\s*,\s*(0|1|0?\.\d+)\s*\)$/.test(
-        value.trim(),
-    );
-
-/**
- * Checks if the input is a valid HSL color.
- * HSL format: hsl(h, s%, l%)
- * where h is an integer between 0 and 360,
- * s and l are percentages between 0% and 100%.
- */
-const isValidHsl = (value: string): boolean =>
-    /^hsl\(\s*\d{1,3}\s*,\s*\d{1,3}%\s*,\s*\d{1,3}%\s*\)$/.test(value.trim());
-
-/**
- * Checks if the input is a valid HSLA color.
- * HSLA format: hsla(h, s%, l%, a)
- * where h is an integer between 0 and 360,
- * s and l are percentages between 0% and 100%,
- * and a is a number between 0 and 1 (inclusive).
- */
-const isValidHsla = (value: string): boolean =>
-    /^hsla\(\s*\d{1,3}\s*,\s*\d{1,3}%\s*,\s*\d{1,3}%\s*,\s*(0|1|0?\.\d+)\s*\)$/.test(
-        value.trim(),
-    );
-
-/**
- * Checks if the input is a valid color input.
- * It checks for valid hex, rgb, rgba, hsl, and hsla formats.
- * Returns true if the input is valid, false otherwise.
- */
-const isValidColorInput = (value: string): boolean =>
-    isValidHex(value) ||
-    isValidRgb(value) ||
-    isValidRgba(value) ||
-    isValidHsl(value) ||
-    isValidHsla(value);
 
 /**
  * Custom hook for managing color input.
@@ -72,13 +23,16 @@ const isValidColorInput = (value: string): boolean =>
  */
 export const useColorInput = <T = ColorLike>(
     alpha = 100,
-    initialColor: ColorLike = randomHexColor(),
+    type: ColorType = "hex",
+    initialColor?: ColorLike,
 ) => {
-    const [inputValue, setInputValue] = useState<string>(initialColor);
-    const [color, setColor] = useState<string>(initialColor);
+    const resolvedColor = initialColor ?? randomColor(type, alpha);
+
+    const [inputValue, setInputValue] = useState<ColorLike>(resolvedColor);
+    const [color, setColor] = useState<ColorLike>(resolvedColor);
     const [isInvalid, setIsInvalid] = useState<boolean>(false);
 
-    const handleChange = (input: string) => {
+    const handleChange = (input: ColorLike) => {
         setInputValue(input);
     };
 
@@ -89,12 +43,17 @@ export const useColorInput = <T = ColorLike>(
             const parsed = parse(trimmed);
 
             if (parsed) {
-                setColor(
-                    alpha === 100
-                        ? formatHex(parsed)
-                        : (formatHex8(alphaUtil(parsed, alpha)) ??
-                              randomHexColor()),
-                );
+                switch (type) {
+                    case "hex":
+                        if (alpha === 100) setColor(formatHex(parsed) as Hex);
+                        else setColor(formatHex8(parsed) as Hex);
+                        break;
+                    case "rgb":
+                        setColor(formatRgb(parsed) as RGB | RGBA);
+                        break;
+                    case "hsl":
+                        setColor(formatHsl(parsed) as HSL | HSLA);
+                }
                 setIsInvalid(false);
 
                 return;
@@ -104,11 +63,21 @@ export const useColorInput = <T = ColorLike>(
         setIsInvalid(true);
     };
 
-    const setColorDirectly = (color: string) => {
+    const setColorDirectly = (color: ColorLike) => {
         setInputValue(color);
         const parsed = parse(color);
         if (parsed) {
-            setColor(alpha === 100 ? formatHex(parsed) : formatHex8(parsed));
+            switch (type) {
+                case "hex":
+                    if (alpha === 100) setColor(formatHex(parsed) as Hex);
+                    else setColor(formatHex8(parsed) as Hex);
+                    break;
+                case "rgb":
+                    setColor(formatRgb(parsed) as RGB | RGBA);
+                    break;
+                case "hsl":
+                    setColor(formatHsl(parsed) as HSL | HSLA);
+            }
             setIsInvalid(false);
         } else {
             setIsInvalid(true);
