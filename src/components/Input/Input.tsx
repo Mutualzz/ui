315727d382369e3837045a0a99d2111cecac7fef
mateie <mateie@gmail.com>
2025-08-13 15:16:18 -0700

    exported all the types for all the input types

--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -28,3 +28,8 @@ export * from "@components/InputColor/InputColor";
 export * from "@components/InputNumber/InputNumber";
 export * from "@components/InputPassword/InputPassword";
 export * from "@components/InputText/InputText";
+
+export type * from "@components/InputColor/InputColor.types";
+export type * from "@components/InputNumber/InputNumber.types";
+export type * from "@components/InputPassword/InputPassword.types";
+export type * from "@components/InputText/InputText.types";
