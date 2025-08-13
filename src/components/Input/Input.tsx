7e1ddb64384ae4ef34b39b802358e5697468c1e5
mateie <mateie@gmail.com>
2025-08-13 14:33:29 -0700

    Exported Individual Inputs from here and updated types

--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -1,9 +1,13 @@
+import { InputColor } from "@components/InputColor/InputColor";
+import type { InputColorProps } from "@components/InputColor/InputColor.types";
 import type { InputNumberProps } from "@components/InputNumber/InputNumber.types";
+import { InputOther } from "@components/InputOther/InputOther";
 import type { InputPasswordProps } from "@components/InputPassword/InputPassword.types";
+import type { InputTextProps } from "@components/InputText/InputText.types";
 import { InputNumber } from "../InputNumber/InputNumber";
 import { InputPassword } from "../InputPassword/InputPassword";
 import { InputText } from "../InputText/InputText";
-import type { InputProps } from "./Input.types";
+import type { InputProps, InputRootProps } from "./Input.types";
 
 export const Input = (props: InputProps) => {
     switch (props.type) {
@@ -11,7 +15,16 @@ export const Input = (props: InputProps) => {
             return <InputNumber {...(props as InputNumberProps)} />;
         case "password":
             return <InputPassword {...(props as InputPasswordProps)} />;
+        case "color":
+            return <InputColor {...(props as InputColorProps)} />;
+        case "text":
+            return <InputText {...(props as InputTextProps)} />;
         default:
-            return <InputText {...props} />;
+            return <InputOther {...(props as InputRootProps)} />;
     }
 };
+
+export * from "@components/InputColor/InputColor";
+export * from "@components/InputNumber/InputNumber";
+export * from "@components/InputPassword/InputPassword";
+export * from "@components/InputText/InputText";
--- src/components/Input/Input.types.ts
+++ src/components/Input/Input.types.ts
@@ -1,29 +1,17 @@
+import type { InputColorProps } from "@components/InputColor/InputColor.types";
 import type { InputNumberProps } from "@components/InputNumber/InputNumber.types";
 import type { InputPasswordProps } from "@components/InputPassword/InputPassword.types";
 import type { InputRootProps } from "@components/InputRoot/InputRoot.types";
+import type { InputTextProps } from "@components/InputText/InputText.types";
 
-export type InputType =
-    | "date"
-    | "datetime-local"
-    | "email"
-    | "month"
-    | "number"
-    | "password"
-    | "search"
-    | "tel"
-    | "text"
-    | "time"
-    | "url"
-    | "week";
+export type InputProps =
+    | InputRootProps
+    | InputColorProps
+    | InputPasswordProps
+    | InputNumberProps
+    | InputTextProps;
 
-export type InputMode =
-    | "none"
-    | "text"
-    | "tel"
-    | "url"
-    | "email"
-    | "numeric"
-    | "decimal"
-    | "search";
-
-export type InputProps = InputRootProps | InputPasswordProps | InputNumberProps;
+export type * from "@components/InputColor/InputColor.types";
+export type * from "@components/InputNumber/InputNumber.types";
+export type * from "@components/InputPassword/InputPassword.types";
+export type * from "@components/InputRoot/InputRoot.types";
