387589414336e1df30b49abb5b2f0a56a20024b7
mateie <mateie@gmail.com>
2025-08-13 15:35:13 -0700

    Made it so users have to excplicitly use certain inputs (better UIX)

--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -1,27 +1,3 @@
-import { InputColor } from "@components/InputColor/InputColor";
-import type { InputColorProps } from "@components/InputColor/InputColor.types";
-import type { InputNumberProps } from "@components/InputNumber/InputNumber.types";
-import type { InputPasswordProps } from "@components/InputPassword/InputPassword.types";
-import type { InputTextProps } from "@components/InputText/InputText.types";
-import { InputNumber } from "../InputNumber/InputNumber";
-import { InputPassword } from "../InputPassword/InputPassword";
-import { InputText } from "../InputText/InputText";
-import type { InputProps } from "./Input.types";
-
-export const Input = (props: InputProps) => {
-    switch (props.type) {
-        case "number":
-            return <InputNumber {...(props as InputNumberProps)} />;
-        case "password":
-            return <InputPassword {...(props as InputPasswordProps)} />;
-        case "color":
-            return <InputColor {...(props as InputColorProps)} />;
-        case "text":
-        default:
-            return <InputText {...(props as InputTextProps)} />;
-    }
-};
-
 export * from "@components/InputColor/InputColor";
 export * from "@components/InputNumber/InputNumber";
 export * from "@components/InputPassword/InputPassword";
--- src/components/Input/Input.types.ts
+++ src/components/Input/Input.types.ts
@@ -1,16 +1,3 @@
-import type { InputColorProps } from "@components/InputColor/InputColor.types";
-import type { InputNumberProps } from "@components/InputNumber/InputNumber.types";
-import type { InputPasswordProps } from "@components/InputPassword/InputPassword.types";
-import type { InputRootProps } from "@components/InputRoot/InputRoot.types";
-import type { InputTextProps } from "@components/InputText/InputText.types";
-
-export type InputProps =
-    | InputRootProps
-    | InputColorProps
-    | InputPasswordProps
-    | InputNumberProps
-    | InputTextProps;
-
 export type * from "@components/InputColor/InputColor.types";
 export type * from "@components/InputNumber/InputNumber.types";
 export type * from "@components/InputPassword/InputPassword.types";
